0e45785e5c538511531863ecdd7093f87369d5bc
Kryptonomic1 <greg@kryptonomic.io>
2023-09-07 23:53:30 -0400

    added /pages and /styles

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+    reactStrictMode: true,
+    // Add your custom configurations here
+  };
+  
---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import Layout from '../app/layout';
+
+const Home: React.FC = () => {
+  return (
+    <Layout title="Home">
+      <h1>Welcome to Nvatomate</h1>
+      <p>This is your homepage.</p>
+    </Layout>
+  );
+};
+
+export default Home;
