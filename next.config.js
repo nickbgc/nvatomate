0ddf4db4ccb38bd246e955ebf854d4b1340ba965
Kryptonomic1 <greg@kryptonomic.io>
2023-09-07 23:57:15 -0400

    updated next.config.js file

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
+const path = require('path');
+
 module.exports = {
-    reactStrictMode: true,
-    // Add your custom configurations here
-  };
-  
+  reactStrictMode: true,
+  env: {
+    CUSTOM_ENV_VARIABLE: process.env.CUSTOM_ENV_VARIABLE,
+  },
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Modify the `config` object as needed
+    return config;
+  },
+};
